f0f989aa2fffb41dff81ab943938170d9381d039
Bilgehan Senay <bilgehannnoe@gmail.com>
2021-05-07 12:18:50 +0300

    Initial Commit

---
+++ Arcelik-framework/Arcelik_framework.h
@@ -0,0 +1,18 @@
+//
+//  Arcelik_framework.h
+//  Arcelik-framework
+//
+//  Created by Bilgehan Senay on 7.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Arcelik_framework.
+FOUNDATION_EXPORT double Arcelik_frameworkVersionNumber;
+
+//! Project version string for Arcelik_framework.
+FOUNDATION_EXPORT const unsigned char Arcelik_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arcelik_framework/PublicHeader.h>
+
+
